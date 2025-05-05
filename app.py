a2e6b210caa60e851d8f659294046379db222bf5
Corolario23 <r.caceres.villar@gmail.com>
2025-05-05 15:53:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
-
 # Modelo para la base de datos
 class CirujanosTurno(db.Model):
     id = db.Column(db.Integer, primary_key=True)
