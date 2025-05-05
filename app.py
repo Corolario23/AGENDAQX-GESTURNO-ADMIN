6ddce8cbcb66be25e191c79914c02fe607087234
Corolario23 <r.caceres.villar@gmail.com>
2025-05-05 15:30:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///turnos.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 # Modelo para la base de datos
 class CirujanosTurno(db.Model):
